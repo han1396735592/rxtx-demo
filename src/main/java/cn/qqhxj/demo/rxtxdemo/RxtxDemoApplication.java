813395902a786ec514ba0ea3cbdf50e95015b089
han1396735592 <1396735592@qq.com>
2018-12-26 23:08:05 +0800

    Initial commit

---
+++ src/main/java/cn/qqhxj/demo/rxtxdemo/RxtxDemoApplication.java
@@ -0,0 +1,22 @@
+package cn.qqhxj.demo.rxtxdemo;
+
+import cn.qqhxj.common.rxtx.processor.SerialDataProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxtxDemoApplication implements SerialDataProcessor<String> {
+    @Override
+    public void processor(String s) {
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(RxtxDemoApplication.class, args);
+        while (true) {
+            ;
+        }
+    }
+
+}
+
