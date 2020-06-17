97764f76ecc3e63540dda613b429d67eac2e5600
韩新建 <1396735592@qq.com>
2020-06-17 20:05:49 +0800

    update pom

--- src/main/java/cn/qqhxj/demo/rxtxdemo/RxtxDemoApplication.java
+++ src/main/java/cn/qqhxj/demo/rxtxdemo/RxtxDemoApplication.java
@@ -1,14 +1,19 @@
 package cn.qqhxj.demo.rxtxdemo;
 
+import cn.qqhxj.common.rxtx.SerialContext;
 import cn.qqhxj.common.rxtx.processor.SerialDataProcessor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class RxtxDemoApplication implements SerialDataProcessor<String> {
     @Override
-    public void process (String s) {
-        System.out.println(s);
+    public void process(String s) {
+        log.info("message received = {}", s);
+        // todo 处理消息 这里做了一个回显
+        SerialContext.sendData(("return " + s).getBytes());
     }
 
     public static void main(String[] args) {
