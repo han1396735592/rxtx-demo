f93aebae54edda6924274f5c05158fb22cd3d32b
韩新建 <1396735592@qq.com>
2023-08-31 13:57:47 +0800

    3.1.0 demo

--- src/main/java/cn/qqhxj/demo/rxtxdemo/RxtxDemoApplication.java
+++ src/main/java/cn/qqhxj/demo/rxtxdemo/RxtxDemoApplication.java
@@ -1,31 +1,57 @@
 package cn.qqhxj.demo.rxtxdemo;
 
-import cn.qqhxj.common.rxtx.SerialContext;
-import cn.qqhxj.common.rxtx.processor.SerialDataProcessor;
-import cn.qqhxj.common.rxtx.reader.AnyDataReader;
-import cn.qqhxj.common.rxtx.reader.SerialReader;
+
+import cn.qqhxj.rxtx.context.SerialContext;
+import cn.qqhxj.rxtx.parse.SerialDataParser;
+import cn.qqhxj.rxtx.parse.StringSerialDataParser;
+import cn.qqhxj.rxtx.processor.SerialDataProcessor;
+import cn.qqhxj.rxtx.starter.annotation.EnableSerialPort;
+import cn.qqhxj.rxtx.starter.annotation.EnableSerialPortAutoConfig;
+import cn.qqhxj.rxtx.starter.annotation.SerialPortBinder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
+import javax.annotation.Resource;
+
+@EnableSerialPortAutoConfig
 @Slf4j
 @SpringBootApplication
+@SerialPortBinder("COM1")
+@EnableSerialPort(value = "COM1", port = "COM1")
+@EnableSerialPort(value = "COM3", port = "COM3")
 public class RxtxDemoApplication implements SerialDataProcessor<String> {
+
+
+    @Lazy
+    @Resource
+    @Qualifier("COM3.SerialContext")
+    private SerialContext serialContext;
 
     // 处理串口接收的数据 默认是 new String(data)
     @Override
-    public void process(String s) {
+    public void process(String s,SerialContext serialContext) {
         log.info("message received = {}", s);
         // todo 处理消息 这里做了一个回显
-        SerialContext.sendData(("return " + s).getBytes());
+        System.out.println(serialContext);
+        serialContext.sendData(("return " + s).getBytes());
     }
 
-    // 可以接受所有的串口数据
+    @SerialPortBinder("COM1")
     @Bean
-    public SerialReader serialReader(){
-        return new AnyDataReader();
+    public SerialDataParser serialDataParser() {
+        return new StringSerialDataParser();
     }
+
+    @SerialPortBinder("COM3")
+    @Bean
+    public SerialDataParser serialDataParser2() {
+        return new StringSerialDataParser();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(RxtxDemoApplication.class, args);
